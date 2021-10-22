b90a6951eaa1efe2083e88c72f88b019a1e434da
monet <monethuang.outlook@com>
2021-10-22 20:57:24 +0800

    hot-fix commit3

--- src/main/java/com/monet/git/GitTest.java
+++ src/main/java/com/monet/git/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git4");
-        System.out.println("hello git9");
+        System.out.println("hello git10");
     }
 
 
