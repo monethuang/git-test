c984c5a9ac49920ceab316652e106ddc7dbeec5b
monet <monethuang.outlook@com>
2021-10-22 20:47:35 +0800

    second commit

--- src/main/java/com/monet/git/GitTest.java
+++ src/main/java/com/monet/git/GitTest.java
@@ -3,6 +3,8 @@ package com.monet.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello git");
+        System.out.println("hello git2");
     }
 
 
